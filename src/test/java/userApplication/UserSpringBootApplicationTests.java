99ea55d3718cde39f1a59d75b444ee5078b15e82
Lwazi <lwazigxogxa@gmail.com>
2023-04-12 16:39:16 +0200

    first commit

---
+++ src/test/java/userApplication/UserSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package userApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
